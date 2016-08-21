ce5f761ce57411357fbd3c537815679d34224290
ducdong <ducdong.mg@gmail.com>
2016-08-21 10:30:49 +0700

    chapter 3 -projectionInNodeJSDriver - Quiz Solved

--- week3/projectionInNodeJSDriver/quiz.js
+++ week3/projectionInNodeJSDriver/quiz.js
@@ -8,11 +8,11 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
 
-    var query = {"founded_year": "2010"};
-    
+    var query = {"founded_year": 2010};
+
     var cursor = db.collection('companies').find(query);
 
-    /* TODO: Write your line of code here. */
+    cursor.project({"name": 1, "number_of_employees": 1, "_id": 0});
 
     cursor.forEach(
         function(doc) {
