30369546cd8c668f8b9a621e2f3fad817b64d437
ducdong <ducdong.mg@gmail.com>
2016-08-21 10:04:26 +0700

    chapter 3 -projectionInNodeJSDriver

--- week3/projectionInNodeJSDriver/app.js
+++ week3/projectionInNodeJSDriver/app.js
@@ -1,3 +1,4 @@
+
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
@@ -8,12 +9,15 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
     console.log("Successfully connected to MongoDB.");
 
     var query = {"category_code": "biotech"};
+    var projection = {"name": 1, "category_code": 1, "_id": 0};
 
     var cursor = db.collection('companies').find(query);
+    cursor.project(projection);
 
     cursor.forEach(
         function(doc) {
-            console.log( doc.name + " is a " + doc.category_code + " company." );
+            console.log(doc.name + " is a " + doc.category_code + " company.");
+            console.log(doc);
         },
         function(err) {
             assert.equal(err, null);
