4f1067d4a2377b95573a1d288ffcbbef13450789
LOCAL\nddong <ducdong.mg@gmail.com>
2016-10-04 08:56:34 +0700

    final - lab 4

--- final/mongomart/items.js
+++ final/mongomart/items.js
@@ -279,18 +279,16 @@ function ItemDAO(database) {
          *
          */
 
-        var reviewDoc = {
-            name: name,
-            comment: comment,
-            stars: stars,
-            date: Date.now()
-        }
-
         // TODO replace the following two lines with your code that will
         // update the document with a new review.
-        // var doc = this.createDummyItem();
-        // doc.reviews = [reviewDoc];
-        this.db.collection("item").update({_id: itemId}, {$push: {reviews: reviewDoc}});
+        this.db.collection("item").update(  {_id: itemId},
+                                            {$push: {reviews:
+                                                        {$each:[
+                                                            {name: name,
+                                                            comment: comment,
+                                                            stars: stars,
+                                                            date: Date.now()}
+                                                            ]}}});
 
         // TODO Include the following line in the appropriate
         // place within your code to pass the updated doc to the
